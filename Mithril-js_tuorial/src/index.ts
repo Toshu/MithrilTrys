1d54b6798cc7df327fb176391fc81e804078f181
Toshu Amashu <toshude@yahoo.de>
2023-05-21 15:21:34 +0200

    init css

--- Mithril-js_tuorial/src/index.ts
+++ Mithril-js_tuorial/src/index.ts
@@ -3,4 +3,6 @@ import m from "mithril";
 import { User } from "./models/User";
 import UserList from "./views/UserList";
 
+import "./styles.css";
+
 m.mount(document.body, new UserList(new User()));
