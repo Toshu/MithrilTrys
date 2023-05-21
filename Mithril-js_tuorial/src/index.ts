6fc0b48966e117944da387f8f0a95910dd425a25
Toshu Amashu <toshude@yahoo.de>
2023-05-21 17:19:25 +0200

    layout with nav-bar

--- Mithril-js_tuorial/src/index.ts
+++ Mithril-js_tuorial/src/index.ts
@@ -2,7 +2,19 @@ import m from "mithril";
 
 import UserList from "./views/UserList";
 import UserForm from "./views/UserForm";
+import Layout from "./views/Layout";
 
 import "./styles.css";
 
-m.route(document.body, "/list", { "/list": UserList, "/edit/:id": UserForm });
+m.route(document.body, "/list", {
+  "/list": {
+    render: function () {
+      return m(Layout, m(UserList));
+    },
+  },
+  "/edit/:id": {
+    render: function (vnode) {
+      return m(Layout, m(UserForm, vnode.attrs));
+    },
+  },
+});
