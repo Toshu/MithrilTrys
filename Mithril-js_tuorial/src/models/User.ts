dd036443784fae0a8c6a3f0140bbcb7452975c0b
Toshu Amashu <toshude@yahoo.de>
2023-05-21 17:08:43 +0200

    save changed user

--- Mithril-js_tuorial/src/models/User.ts
+++ Mithril-js_tuorial/src/models/User.ts
@@ -13,6 +13,12 @@ interface UserData {
   lastName: String;
 }
 
+const startUsers: UserData[] = [];
+startUsers.push({ id: "1", firstName: "fn001", lastName: "001" });
+startUsers.push({ id: "2", firstName: "fn002", lastName: "002" });
+startUsers.push({ id: "3", firstName: "fn003", lastName: "003" });
+startUsers.push({ id: "4", firstName: "fn004", lastName: "004" });
+
 const User = {
   userList: [] as UserData[],
 
@@ -29,10 +35,12 @@ const User = {
     //     User.userList = (result as UserAPI).data;
     //   });
 
-    User.userList.push({ id: "1", firstName: "fn001", lastName: "001" });
-    User.userList.push({ id: "2", firstName: "fn002", lastName: "002" });
-    User.userList.push({ id: "3", firstName: "fn003", lastName: "003" });
-    User.userList.push({ id: "4", firstName: "fn004", lastName: "004" });
+    User.userList = [] as UserData[];
+    startUsers.forEach((u) => User.userList.push(u));
+    // User.userList.push({ id: "1", firstName: "fn001", lastName: "001" });
+    // User.userList.push({ id: "2", firstName: "fn002", lastName: "002" });
+    // User.userList.push({ id: "3", firstName: "fn003", lastName: "003" });
+    // User.userList.push({ id: "4", firstName: "fn004", lastName: "004" });
   },
 
   current: {} as UserData | undefined,
@@ -48,6 +56,29 @@ const User = {
     //   });
 
     User.current = User.userList.find((u) => u.id === id);
+  },
+
+  save: function () {
+    // return m.request({
+    //   method: "PUT",
+    //   url: "https://rem-rest-api.herokuapp.com/api/users/" + User.current?.id,
+    //   body: User.current,
+    //   withCredentials: true,
+    // });
+
+    // console.log("save 1");
+    if (User.current) {
+      //   console.log("save 2");
+      const startuser = startUsers.find((u) => u.id === User.current?.id);
+      if (startuser) {
+        // console.log("save 3");
+        // console.log("user:", startuser);
+        startuser.firstName = User.current.firstName;
+        startuser.lastName = User.current.lastName;
+        // console.log("save 4");
+        // console.log("user:", startuser);
+      }
+    }
   },
 };
 
--- Mithril-js_tuorial/src/views/UserForm.ts
+++ Mithril-js_tuorial/src/views/UserForm.ts
@@ -7,17 +7,50 @@ const UserForm: m.Component = {
   },
 
   view: () => {
-    return m("form", [
-      m("label.label", "First name"),
-      m("input.input[type=text][placeholder=First name]", {
-        value: User.current?.firstName,
-      }),
-      m("label.label", "Last name"),
-      m("input.input[placeholder=Last name]", {
-        value: User.current?.lastName,
-      }),
-      m("button.button[type=submit]", "Save"),
-    ]);
+    return m(
+      "form",
+      {
+        onsubmit: (e: Event) => {
+          e.preventDefault();
+          User.save();
+        },
+      },
+      [
+        m("label.label", "First name"),
+        m("input.input[type=text][placeholder=First name]", {
+          oninput: (e: Event) => {
+            // console.log("Event: ", e);
+            if (User.current) {
+              User.current.firstName = (e.target as HTMLInputElement).value;
+            } else {
+              User.current = {
+                id: "93",
+                firstName: (e.target as HTMLInputElement).value,
+                lastName: "unknown",
+              };
+            }
+          },
+          value: User.current?.firstName,
+        }),
+        m("label.label", "Last name"),
+        m("input.input[placeholder=Last name]", {
+          oninput: (e: Event) => {
+            // console.log("Event: ", e);
+            if (User.current) {
+              User.current.lastName = (e.target as HTMLInputElement).value;
+            } else {
+              User.current = {
+                id: "93",
+                firstName: "unknown",
+                lastName: (e.target as HTMLInputElement).value,
+              };
+            }
+          },
+          value: User.current?.lastName,
+        }),
+        m("button.button[type=submit]", "Save"),
+      ]
+    );
   },
 };
 
