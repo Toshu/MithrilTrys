2d0a0ad5c67f997b81422d8423099933757954d3
Toshu Amashu <toshude@yahoo.de>
2023-05-22 20:33:29 +0200

    replace default content with app specific content

--- try_mithril.js/src/main.ts
+++ try_mithril.js/src/main.ts
@@ -1,26 +1,16 @@
 import "./style.css";
-// get asset as source
-import typescriptLogo from "./typescript.svg";
-// get asset as public static element
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+import m from "mithril";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+function App(): m.Component {
+  return {
+    view: () =>
+      m(
+        "div.app",
+        m("header", "Mithril Demo"),
+        m("main", "[some content]"),
+        m("footer", "Mithril is simple yet powerful")
+      ),
+  };
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+m.mount(document.body, App);
