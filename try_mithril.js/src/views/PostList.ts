9c65c7bb3ac0ef4d10adf246cedb5457c41b8f2f
Toshu Amashu <toshude@yahoo.de>
2023-05-22 21:02:01 +0200

    using dynamic post data

--- try_mithril.js/src/views/PostList.ts
+++ try_mithril.js/src/views/PostList.ts
@@ -1,7 +1,53 @@
 import m from "mithril";
 
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+let posts: IPost[] = [];
+
 const PostList: m.Component = {
-  view: () => "Post list",
+  oninit: () => {
+    // m.request<IPost[]>({
+    //   method: "GET",
+    //   url: "https://jsonplaceholder.typicode.com/posts",
+    // }).then((data) => (posts = data as IPost[]));
+    posts = [
+      { userId: 1, id: 1, title: "Dummy post 1", body: "dummy post 1" },
+      { userId: 1, id: 2, title: "Dummy post 2", body: "dummy post 2" },
+    ];
+  },
+
+  view: () =>
+    m(
+      "section.post-list",
+      m(
+        "ul",
+        posts.map((post) =>
+          m(
+            "li",
+            m(
+              m.route.Link,
+              {
+                href: `/posts/${post.id}`,
+              },
+              post.title
+            )
+          )
+        )
+      ),
+      m(
+        m.route.Link,
+        {
+          href: "/posts",
+          className: "button",
+        },
+        "Add new post"
+      )
+    ),
 };
 
 export default PostList;
